7fa227eb24e5c92df2386c80d28dd1d095a482e7
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2012-06-09 21:23:45 +0200

    Changes in #26

--- backend/mods/units.php
+++ backend/mods/units.php
@@ -2,13 +2,26 @@
 
 function move($db){
 	$move = $_POST['move'];
-	insert($db, $move);
-	if($move['freeunits']>0){
-		$free = $move;
-		$free['units'] = $free['freeunits'];
-		$free['pricebuy'] = 0;
-		insert($db, $free);
-	};
+	switch($move['type']){
+		case "in_buy":
+			insert($db, $move);
+			if($move['freeunits']>0){
+				$free = $move;
+				$free['units'] = $free['freeunits'];
+				$free['pricebuy'] = 0;
+				insert($db, $free);
+			};
+			$oldpricebuy = mysql_result(mysql_query("SELECT pricebuy FROM distributors_products WHERE distributor_id = '".$move['dist']."' AND product_id = '".$move['prod']."'", $db), 0);
+			if($oldpricebuy!=$move['pricebuy'])mysql_query("UPDATE distributors_products SET pricebuy = '".$move['pricebuy']."' WHERE distributor_id = '".$move['dist']."' AND product_id = '".$move['prod']."'", $db);
+			break;
+		case "in_trans":
+		
+			
+		case "out_sell":
+			withdraw($db, $move);
+			break;
+	}
+
 }
 
 function insert($db, $move){
@@ -19,4 +32,8 @@ function insert($db, $move){
 			(".$move['prod'].", ".$move['dist'].", ".$move['pricebuy'].", '".date("Y-m-d" ,strtotime($move['expiry']))."')";
 		mysql_query($sql, $db);
 	}
+}
+
+function withdraw($db, $move){
+	
 }
