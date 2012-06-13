a5bbddd0c958c535571ef6a108da847a09e0327b
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2012-06-13 18:31:40 +0200

    Small fix on cash add

--- cnts/cash.cnt.php
+++ cnts/cash.cnt.php
@@ -8,7 +8,7 @@ class Cash extends Cnt{
 	
 	public function add($value, $caption){
 		$sql = "INSERT INTO cashposts (value, caption) VALUES ('".$value."', '".$caption."')";
-		mysql_query($sql);
+		mysql_query($sql, $this->app->db);
 	}
 	
 }
