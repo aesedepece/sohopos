c1c4c1a5cad224caad0a3fbf7d8a928f7cf00e97
Adán Sánchez de Pedro Crespo <pedrocrespo@kadede.com>
2012-06-13 18:46:19 +0200

    Rebranding

--- res/php/head.php
+++ res/php/head.php
@@ -2,7 +2,7 @@
 <html lang="es-ES">
 <head>
 	<meta charset="UTF-8">
-	<title>librePOS - Lightweight Point of Sale</title>
+	<title>SOHOPOS - Lightweight Point of Sale</title>
 	<link rel="shortcut icon" href="<?php echo$app->s['r']; ?>res/img/favicon.png" type="image/png" />
 	<script type="text/javascript" src="<?php echo$app->s['r']; ?>res/js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo$app->s['r']; ?>res/js/common.php"></script>
